29b035ff0b2198bbed6387e042f9f68e2155b71e
Adam C. Saunders <saunders@cs.umass.edu>
2021-12-09 12:35:14 -0500

    jest config updates (in progress)

--- jest.base.js
+++ jest.base.js
@@ -3,7 +3,6 @@ const { compilerOptions } = require("../../tsconfig.json");
 
 function rootPath(pk, relpath) {
   const packagePath = pk.name.split('/')[1];
-  console.log({ packagePath })
   if (relpath) {
     return ['<rootDir>', '..', packagePath, relpath].join('/');
   }
@@ -12,17 +11,23 @@ function rootPath(pk, relpath) {
 
 function makeConfig(modulePackage) {
   const displayName = modulePackage.name;
+  // console.log({ displayName })
+  const packagePath = displayName.split('/')[1];
 
   const moduleMap = pathsToModuleNameMapper(compilerOptions.paths, {
     // This has to match the baseUrl defined in tsconfig.json.
-    prefix: "<rootDir>/..",
+    prefix: "<rootDir>/../..",
   });
   const tildePathMap = rootPath(modulePackage, 'src/$1');
+  // const tildePathMap = rootPath(modulePackage, 'src');
   const tsconfig = rootPath(modulePackage, 'tsconfig.json');
+  // const tsconfig = `<rootDir>/../${packagePath}/tsconfig.json`
   const pkgRoot = rootPath(modulePackage);
   const config = {
-    rootDir: '../eslint-config',
-    roots: [pkgRoot],
+    preset: 'ts-jest',
+    rootDir: '../eslint-config/',
+    // roots: [pkgRoot],
+    roots: ['<rootDir>/../..'],
     displayName,
     transform: {
       "^.+\\.ts$": "ts-jest",
@@ -31,29 +36,20 @@ function makeConfig(modulePackage) {
     moduleNameMapper: {
       ...moduleMap,
       "^~/(.*)$": tildePathMap,
+      // "^~$": tildePathMap,
     },
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     globals: {
       'ts-jest': {
-        tsconfig
+        tsconfig,
+        diagnostics: false
       }
     },
   };
-
   console.log({ config })
   return config;
 }
 
 module.exports = {
-
   makeConfig
-  // transform: {
-  //   "^.+\\.ts$": "ts-jest",
-  // },
-  // testRegex: ".*\\.test\\.ts$",
-  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-  //   // This has to match the baseUrl defined in tsconfig.json.
-  //   prefix: "<rootDir>/../..",
-  // }),
-  // moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
