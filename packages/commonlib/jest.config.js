426fa15b9addfa622dfc2407adc3392df5d566de
Adam C. Saunders <saunders@cs.umass.edu>
2021-12-08 20:55:38 -0500

    jest config updates (in progress)

--- packages/commonlib/jest.config.js
+++ packages/commonlib/jest.config.js
@@ -1,24 +1,28 @@
-const baseConfig = require("../eslint-config/jest.base");
+// const baseConfig = require("../eslint-config/jest.base");
+const { makeConfig } = require("../eslint-config/jest.base");
 const pkg = require('./package.json');
 // import { pathsToModuleNameMapper } from "ts-jest";
 // import { compilerOptions } from "../../tsconfig.json";
 
-module.exports = {
-  ...baseConfig,
-  displayName: pkg.name,
-  rootDir: '../eslint-config',
-  roots:['<rootDir>/../commonlib'],
-  moduleNameMapper: {
-    ...baseConfig.moduleNameMapper,
-    // "^~/*": "<rootDir>/../commonlib/src/$1",
-   "^~/(.*)$": "<rootDir>/../commonlib/src/$1",
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/../commonlib/tsconfig.json'
-    }
-  },
-  // setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each'],
-  // snapshotSerializers: ['jest-emotion']
-};
+// const pkgPath = pkg.name.split('/')[1];
+// const pkgRoot = '<rootDir>/../'+pkgPath;
+// console.log({ name: pkg.name, pkgPath })
 
+module.exports = makeConfig(pkg, '<rootDir>');
+// module.exports = {
+//   ...baseConfig,
+//   displayName: pkg.name,
+//   rootDir: '../eslint-config',
+//   roots:[pkgRoot],
+//   moduleNameMapper: {
+//     ...baseConfig.moduleNameMapper,
+//    "^~/(.*)$": pkgRoot+"/src/$1",
+//   },
+//   globals: {
+//     'ts-jest': {
+//       tsconfig: pkgRoot+'/tsconfig.json'
+//     }
+//   },
+//   // setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each'],
+//   // snapshotSerializers: ['jest-emotion']
+// };
--- packages/eslint-config/jest.base.js
+++ packages/eslint-config/jest.base.js
@@ -1,14 +1,59 @@
 const { pathsToModuleNameMapper } = require("ts-jest");
 const { compilerOptions } = require("../../tsconfig.json");
 
-module.exports = {
-  transform: {
-      "^.+\\.ts$": "ts-jest",
-  },
-  testRegex: ".*\\.test\\.ts$",
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+function rootPath(pk, relpath) {
+  const packagePath = pk.name.split('/')[1];
+  console.log({ packagePath })
+  if (relpath) {
+    return ['<rootDir>', '..', packagePath, relpath].join('/');
+  }
+  return ['<rootDir>', '..', packagePath].join('/');
+}
+
+function makeConfig(modulePackage) {
+  const displayName = modulePackage.name;
+
+  const moduleMap = pathsToModuleNameMapper(compilerOptions.paths, {
     // This has to match the baseUrl defined in tsconfig.json.
-    prefix: "<rootDir>/../..",
-  }),
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    prefix: "<rootDir>/..",
+  });
+  const tildePathMap = rootPath(modulePackage, 'src/$1');
+  const tsconfig = rootPath(modulePackage, 'tsconfig.json');
+  const pkgRoot = rootPath(modulePackage);
+  const config = {
+    rootDir: '../eslint-config',
+    roots: [pkgRoot],
+    displayName,
+    transform: {
+      "^.+\\.ts$": "ts-jest",
+    },
+    testRegex: ".*\\.test\\.ts$",
+    moduleNameMapper: {
+      ...moduleMap,
+      "^~/(.*)$": tildePathMap,
+    },
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    globals: {
+      'ts-jest': {
+        tsconfig
+      }
+    },
+  };
+
+  console.log({ config })
+  return config;
+}
+
+module.exports = {
+
+  makeConfig
+  // transform: {
+  //   "^.+\\.ts$": "ts-jest",
+  // },
+  // testRegex: ".*\\.test\\.ts$",
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+  //   // This has to match the baseUrl defined in tsconfig.json.
+  //   prefix: "<rootDir>/../..",
+  // }),
+  // moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
