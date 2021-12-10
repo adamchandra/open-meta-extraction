651b57449fa6d88ce775152f633bddc48e142413
Adam C. Saunders <saunders@cs.umass.edu>
2021-12-10 17:49:50 -0500

    fixed jest configs

--- packages/commonlib/jest.config.js
+++ packages/commonlib/jest.config.js
@@ -1,7 +1,4 @@
-const {
-  makeConfig
-} = require("../root/jest.base");
+const { makeConfig } = require("../root/jest.base");
+const pkg = require('./package.json');
 
-const pkg = require("./package.json");
-
-module.exports = makeConfig(pkg, true);
+module.exports = makeConfig(pkg);
