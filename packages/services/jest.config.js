e9167722e492df3d075f2744a028b03ba01a1e62
Adam C. Saunders <saunders@cs.umass.edu>
2021-12-10 17:49:50 -0500

    fixed jest configs

--- packages/services/jest.config.js
+++ packages/services/jest.config.js
@@ -1,4 +1,4 @@
 const { makeConfig } = require("../root/jest.base");
 const pkg = require('./package.json');
 
-module.exports = makeConfig(pkg, '<rootDir>');
+module.exports = makeConfig(pkg);
--- packages/spider/jest.config.js
+++ packages/spider/jest.config.js
@@ -1,4 +1,4 @@
 const { makeConfig } = require("../root/jest.base");
-// const pkg = require('./package.json');
+const pkg = require('./package.json');
 
-module.exports = makeConfig('.', true);
+module.exports = makeConfig(pkg);
