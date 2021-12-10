731d91e5fc2c87b5da5588657b7dd78debe6cec2
Adam C. Saunders <saunders@cs.umass.edu>
2021-12-10 17:44:22 -0500

    removing unused files

--- packages/root/jest.base.js
+++ packages/root/jest.base.js
@@ -1,5 +1,3 @@
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { compilerOptions } = require("../../tsconfig.json");
 
 function rootPath(packagePath, relpath) {
   if (relpath) {
@@ -8,36 +6,23 @@ function rootPath(packagePath, relpath) {
   return ['<rootDir>', packagePath].join('/');
 }
 
-function makeConfig(modulePackage, local=false) {
-  // const modulePackagePath = [moduleRoot, 'package.json'].join('/');
-  // const modulePackage = require(modulePackagePath);
+function makeConfig(modulePackage) {
   const moduleRoot = '.'
   const displayName = modulePackage.name;
 
-  const moduleMap = pathsToModuleNameMapper(compilerOptions.paths, {
-    // This has to match the baseUrl defined in tsconfig.json.
-    prefix: "<rootDir>/../..",
-  });
   const tildePathMap = rootPath(moduleRoot, 'src/$1');
   const tsconfig = rootPath(moduleRoot, 'tsconfig.json');
-  const pkgRoot = rootPath(moduleRoot);
+
   const config = {
     preset: 'ts-jest',
     rootDir: '.',
-    // roots: [pkgRoot],
     roots: ['<rootDir>/src'],
-    // resolver: '<rootDir>/resolver.js',
     displayName,
-    // transform: {
-    //   "^.+\\.ts$": "ts-jest",
-    // },
     testRegex: ".*\\.test\\.ts$",
     moduleNameMapper: {
-      // ...moduleMap,
       "^~/(.*)$": tildePathMap,
     },
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    // transformIgnorePatterns: [ "commonlib" ],
     globals: {
       'ts-jest': {
         tsconfig,
@@ -47,7 +32,6 @@ function makeConfig(modulePackage, local=false) {
       }
     },
   };
-  console.log({ config })
   return config;
 }
 
