2f3c00f3a665de1cb3aedb435052e68118e31722
Adam C. Saunders <saunders@cs.umass.edu>
2022-05-25 09:24:08 -0400

    file refactoring (wip)

--- packages/spider/src/predef/task-defs.ts
+++ packages/spider/src/predef/task-defs.ts
@@ -1,10 +1,7 @@
 import _ from 'lodash';
 
-import { flow as fptsFlow, pipe } from 'fp-ts/function';
-
 import { Logger } from 'winston';
 import { Page } from 'puppeteer';
-
 
 import { taskflow as ft } from '@watr/commonlib';
 import { BrowserInstance, BrowserPool } from '~/browser-pool';
@@ -29,3 +26,33 @@ export interface ExtractionEnv extends ExtractionSharedEnv {
 };
 
 const fp = ft.createFPackage<ExtractionEnv>();
+
+export const {
+  tap,
+  tapLeft,
+  tapEitherEnv,
+  through,
+  log,
+  filter,
+  ClientFunc,
+  Transform,
+  forEachDo,
+  attemptEach,
+  takeWhileSuccess,
+  collectFanout,
+} = fp;
+
+// export type ControlInstruction = ft.ControlInstruction;
+
+type EnvT = ExtractionEnv;
+
+export type ExtractionTask<A> = ft.ExtractionTask<A, EnvT>;
+// export type PerhapsW<A> = ft.PerhapsW<A, EnvT>;
+// export type ClientFunc<A, B> = ft.ClientFunc<A, B, EnvT>;
+// export type ClientResult<A> = ft.ClientResult<A>;
+export type Transform<A, B> = ft.Transform<A, B, EnvT>;
+export type FilterTransform<A> = ft.FilterTransform<A, EnvT>;
+
+export type ExtractionRule = (ra: ExtractionTask<unknown>) => ExtractionTask<unknown>;
+
+export const compose = ft.compose;
